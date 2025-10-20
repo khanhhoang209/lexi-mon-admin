3c9cbb83b5d3546b794bfa5405d62dc3324d13c2
khanhhoang209 <lehoangkhanh209@gmail.com>
2025-10-20 15:38:51 +0700

    feat: add revenue by weekly, monthly, year

--- src/types/dashboard.tsx
+++ src/types/dashboard.tsx
@@ -27,3 +27,42 @@ export interface CoinData {
   courseCoin: number
   totalCoin: number
 }
+
+export interface DayRevenueData {
+  itemRevenue: number
+  courseRevenue: number
+  premiumRevenue: number
+  totalRevenue: number
+}
+
+export interface WeeklyRevenueData {
+  monday: DayRevenueData
+  tuesday: DayRevenueData
+  wednesday: DayRevenueData
+  thursday: DayRevenueData
+  friday: DayRevenueData
+  saturday: DayRevenueData
+  sunday: DayRevenueData
+}
+
+export interface MonthRevenueData {
+  itemRevenue: number
+  courseRevenue: number
+  premiumRevenue: number
+  totalRevenue: number
+}
+
+export interface YearlyRevenueData {
+  january: MonthRevenueData
+  february: MonthRevenueData
+  march: MonthRevenueData
+  april: MonthRevenueData
+  may: MonthRevenueData
+  june: MonthRevenueData
+  july: MonthRevenueData
+  august: MonthRevenueData
+  september: MonthRevenueData
+  october: MonthRevenueData
+  november: MonthRevenueData
+  december: MonthRevenueData
+}
